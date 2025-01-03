2e2125c1fbe6cf08ff6660018f9c822273a14eef
bensonc27 <bensonc27@nycstudents.net>
2025-01-03 12:28:22 -0500

    comments

--- task/JS/main.js
+++ task/JS/main.js
@@ -1,3 +1,9 @@
+const DomSelectors = {
+  container: document.querySelector(".container"),
+  search: document.getElementById("search"),
+  button: document.getElementById("button"),
+};
+
 async function getData() {
   try {
     const response = await fetch("");
@@ -17,5 +23,24 @@ async function getData() {
 //Cards should have title and image. Under the cards should be a submit button
 //Correct card remains, the other card is removed
 //If wrong card is chosen
-function higherview() {}
-// find which movie higher view
+function Createcard() {
+  DomSelectors.container.innerHTML = "";
+
+  data.array.forEach((element) => {
+    const CardHTML = 
+// include movie name and image
+// create a submit button
+  });
+}
+
+function higherview() {
+// assign the movie views to the submit button and set that as the value
+// if the views of the movie on card A > the views of the movie on card B, return A
+// else return card B
+// const bigMovie = the larger movie
+}
+
+// add event listener to the button A and button B
+// if the user selects the correct button, then the card stays on the page and the other card is removed.
+// if the user selects the wrong button, then the container is cleared and an alert message pops up saying "Wrong Choice, Restart."
+
